98d3dce3c41365b3148b000831da05a6052a1695
Cameron Raymond <cameronraymond698@gmail.com>
2022-02-10 22:01:37 -0800

    added tests

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(max_length=200)
     
     def __str__(self):
         return self.text[:50]
